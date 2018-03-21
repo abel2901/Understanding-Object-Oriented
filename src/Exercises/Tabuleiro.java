ad2f71aff3be1806f3866d08c33400439bc1194d
abelaraujo <abel2901@gmail.com>
2018-03-21 06:15:07 -0300

    Add Tabuleiro

---
+++ src/Exercises/Tabuleiro.java
@@ -0,0 +1,21 @@
+/*
+ * * * * * * * *
+  * * * * * * * *
+ * * * * * * * * *
+  * * * * * * * * *
+ */
+package Exercises;
+
+public class Tabuleiro {
+    public static void main(String[] args){
+        for(int i=0; i<7; i++){
+            for(int j=0; j<7; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+            if(i % 2 == 0)
+                System.out.print(" ");
+        }
+    }
+    
+}
