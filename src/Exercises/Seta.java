62f61a70cea6c62f9aa0f6e00594b6d128e101c8
abelaraujo <abel2901@gmail.com>
2018-03-25 11:12:17 -0300

    Updating Seta class

--- src/Exercises/Seta.java
+++ src/Exercises/Seta.java
@@ -1,37 +1,36 @@
-
 package Exercises;
 
 public class Seta {
+
     public static void main(String[] args) {
-       
-        for(int i = 0; i < 9; i++){ 
+
+        for (int i = 0; i < 9; i++) {
             System.out.print("\n");
-            for(int j = 0; j < 9; j++){
-                if(j==4){
+            for (int j = 0; j < 9; j++) {
+                if (j == 4) {
                     System.out.print("*");
                     continue;
                 }
-                
-                if((j==5) && (i>=1 && i<=2)){
+
+                if ((j == 5) && (i >= 1 && i <= 2)) {
                     System.out.print("*");
                     continue;
                 }
-                
-                if((j==6) && (i==2)){
+
+                if ((j == 6) && (i == 2)) {
                     System.out.print("*");
                     continue;
                 }
-                
-                if((j==3)&&(i>=1 && i<=2)){
+
+                if ((j == 3) && (i >= 1 && i <= 2)) {
                     System.out.print("*");
                     continue;
                 }
-                
-                if((j==2) &&(i==2)){
+
+                if ((j == 2) && (i == 2)) {
                     System.out.print("*");
-                }
-                
-                else{
+                    continue;
+                } else {
                     System.out.print(" ");
                 }
             }
